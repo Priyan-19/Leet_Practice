5ca04bb565f0f7bc4032b3447f71736dd8aba471
Priyan-19 <priyan190406@gmail.com>
2026-07-01 14:41:33 +0530

    Time: 0 ms (100%), Space: 19.2 MB (85.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        te=0
         if n==1:
             return 1
-        elif n==2:
-            return 2
-        else :
-            f=1
-            s=2
-            for i in range(3,n+1):
-                c=f+s
-                f=s
-                s=c
-            te=c
-        return (te)
+        te=0
+        f=0
+        s=1
+        for i in range(1,n+1):
+            c=f+s
+            f=s
+            s=c
+        te=c
+        return te
