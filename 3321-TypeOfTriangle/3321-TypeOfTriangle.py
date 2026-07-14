f61dffe667bbf31b3ea8435f476bc5e8207ea1c6
Priyan-19 <priyan190406@gmail.com>
2026-07-14 11:57:27 +0530

    Create 3321-TypeOfTriangle

---
+++ 3321-TypeOfTriangle/3321-TypeOfTriangle.py
@@ -0,0 +1,18 @@
+# Last updated: 7/14/2026, 11:57:27 AM
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        if nums[0] <= 0 or nums[1] <= 0 or nums[2] <= 0:
+            return("Invalid Input")
+        elif nums[0] + nums[1] > nums[2] and nums[1] + nums[2] > nums[0] and nums[0] + nums[2] > nums[1]:
+    
+            if nums[0] == nums[1] == nums[2]:
+                return("equilateral")
+        
+            elif nums[0] == nums[1] or nums[1] == nums[2] or  nums[0] == nums[2]:
+                return("isosceles")
+        
+            else:
+                return("scalene")
+
+        else:
+            return("none")
