33d30f7226c4cdbbd0695eab5922b4738cebf493
Priyan-19 <priyan190406@gmail.com>
2026-07-14 11:58:10 +0530

    Create 152-MaximumProductSubarray

---
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.py
@@ -0,0 +1,12 @@
+# Last updated: 7/14/2026, 11:58:10 AM
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        cur_max = cur_min = result = nums[0]
+        
+        for n in nums[1:]:
+            temp = cur_max
+            cur_max = max(n, n * cur_max, n * cur_min)
+            cur_min = min(n, n * temp, n * cur_min)
+            result = max(result, cur_max)
+        
+        return result
