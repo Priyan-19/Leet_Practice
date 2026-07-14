b3ed1e279912cc5f2813c22c2ee2c7c939d40d62
Priyan-19 <priyan190406@gmail.com>
2026-07-14 11:58:03 +0530

    Create 171-ExcelSheetColumnNumber

---
+++ 171-ExcelSheetColumnNumber/171-ExcelSheetColumnNumber.py
@@ -0,0 +1,8 @@
+# Last updated: 7/14/2026, 11:58:03 AM
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        re=0
+        for i in columnTitle:
+            t = ord(i)-(ord("A")-1)
+            re = re*26+t
+        return re
