31d0ee755d6d53ab2157509b3f652f686913cbf1
Priyan-19 <priyan190406@gmail.com>
2026-07-14 11:57:38 +0530

    Create 414-ThirdMaximumNumber

---
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.py
@@ -0,0 +1,8 @@
+# Last updated: 7/14/2026, 11:57:38 AM
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums=list(set(nums))
+        nums.sort(reverse=True)
+        if len(nums)<=2:
+            return nums[0]
+        return nums[2]
