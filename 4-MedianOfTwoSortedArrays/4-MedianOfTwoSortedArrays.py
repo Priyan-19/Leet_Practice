4acf6fd8df8d7484f6a6c9736787f0c9280ac6f1
Priyan-19 <priyan190406@gmail.com>
2026-07-14 11:58:35 +0530

    Create 4-MedianOfTwoSortedArrays

---
+++ 4-MedianOfTwoSortedArrays/4-MedianOfTwoSortedArrays.py
@@ -0,0 +1,12 @@
+# Last updated: 7/14/2026, 11:58:35 AM
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        arr = nums1 + nums2
+        arr.sort()
+
+        n = len(arr)
+
+        if n % 2 == 1:
+            return arr[n // 2]
+        else:
+            return (arr[n // 2 - 1] + arr[n // 2]) / 2
