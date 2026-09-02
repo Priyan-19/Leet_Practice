b1b7344259a579ccdb08e78fb4fd987a51d9915b
Priyan-19 <priyan190406@gmail.com>
2026-09-02 12:14:59 +0530

    Create 2343-QueryKthSmallestTrimmedNumber

---
+++ 2343-QueryKthSmallestTrimmedNumber/version/python/2343-QueryKthSmallestTrimmedNumber_20260902_121459.py
@@ -0,0 +1,17 @@
+# Last updated: 9/2/2026, 12:14:59 PM
+1class Solution:
+2    def smallestTrimmedNumbers(self, nums: List[str], queries: List[List[int]]) -> List[int]:
+3        result = []
+4
+5        for k, trim in queries:
+6            arr = []
+7
+8            for i, num in enumerate(nums):
+9                trimmed = num[-trim:]
+10                arr.append((trimmed, i))
+11
+12            arr.sort()
+13
+14            result.append(arr[k - 1][1])
+15
+16        return result
